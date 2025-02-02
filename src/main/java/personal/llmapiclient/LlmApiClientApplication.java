85a67bdc2f2c84cb17e7f3672f926e2a57bd843f
hellmir <hellmir610@gmail.com>
2025-02-02 19:11:23 +0900

    [LSAC-002] chore(configuration): Configure initial environment settings and preferences (#2)
    - .gitignore
    - build.gradle
    - LlmApiClientApplication.java
    - application.yml

---
+++ src/main/java/personal/llmapiclient/LlmApiClientApplication.java
@@ -0,0 +1,11 @@
+package personal.llmapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmApiClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LlmApiClientApplication.class, args);
+    }
+}
