88b449676bfcb998a99017bb72e73b0d0130968a
hellmir <hellmir610@gmail.com>
2025-02-02 19:25:26 +0900

    [LSAC-007] chore(logging): Add logging aspect and logging interceptor for horizontal logging (#4)
    - LoggingAspect.java
    - InterceptorConfig.java
    - LoggingInterceptor.java
    - LogConstant.java
    - PerformenceMeasurer.java
    - application.yml
    - logback-spring.xml

---
+++ src/main/java/personal/llmapiclient/util/LogConstant.java
@@ -0,0 +1,8 @@
+package personal.llmapiclient.util;
+
+public class LogConstant {
+    public static final String PERFORMANCE_MEASUREMENT = "estimated time: {} ms, used memory: {} bytes";
+
+    public static final String LOG_ERROR_MESSAGE = "Error exception occurred: {}";
+    public static final String LOG_WARN_MESSAGE = "Warning exception occurred: {}";
+}
